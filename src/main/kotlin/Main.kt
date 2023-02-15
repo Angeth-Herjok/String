65cea92aa9672aca9ae5d69d62cdae68ddc5b878
Angeth-Herjok <angethherjok@gmail.com>
2023-02-15 14:39:14 +0300

    string

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+fun main() {
+        //num 1
+    var name = "Akirachix"
+    println(name[0])
+    println(name[2])
+    println(name[3])
+    println(sentence("Angeth", 20))
+    company()
+    //4
+    val name1 = "Becky.That's me! "
+    val name2 = "Cynthia.I don't know who that is"
+    println (name1 + name2)
+
+
+}
+   //num 3
+fun company(){
+    var company = "Bank"
+    return (println("The length is "+ company.length))
+
+
+}
+    //num 2
+fun sentence(x:String, y:Int):String{
+    return "Hi,my $x is and i am $y years old "
+
+
+}
+
